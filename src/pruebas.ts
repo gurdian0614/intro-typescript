21558b8f46cc0e50dbcceed3d9a4d61b4bc72589
gurdian0614 <ivan.gurdian@outlook.com>
2025-10-30 19:54:24 -0600

    Adding data

--- src/pruebas.ts
+++ src/pruebas.ts
@@ -1,13 +1,15 @@
 import {
     actualizarEstado,
+    filtrarPeliculasPorGenero,
     type Pelicula,
     type Reserva,
     crearReserva,
-    type filtrarPeliculasPorGenero,
 } from './reservaciones';
 
 const carteleraInicial: Pelicula[] = [
     { id: 1, titulo: 'El Ultimo Robo', genero: 'Acción', duracionMinutos: 110, es3D: false },
+    { id: 2, titulo: 'Happy Gilmore', genero: 'Comedia', duracionMinutos: 150, es3D: true },
+    { id: 3, titulo: 'Y donde estan las Rubias', genero: 'Comedia', duracionMinutos: 150, es3D: true },
 ];
 console.log('Cartelera cargada: ', carteleraInicial);
 
@@ -20,3 +22,13 @@ const miReservaConfirmada: Reserva = actualizarEstado(miReservaPendiente, 'Confi
 console.log('\n3. Estado Actualizado:');
 console.log('Reserva anterior (Pendiente):', miReservaPendiente.estado);
 console.log('Reserva nueva (Confirmada):', miReservaConfirmada.estado);
+
+// DEMO DE ERROR
+// actualizarEstado(miReservaConfirmada, 'Comprada');
+
+const peliculasAccion = filtrarPeliculasPorGenero(carteleraInicial, 'Comedia');
+
+console.log('\n4. Películas de Acción:');
+peliculasAccion.forEach(p => console.log(` - ${p.titulo}`));
+
+console.log('\n--- PRUEBAS DE TYPESCRIPT FINALIZADAS ---');
